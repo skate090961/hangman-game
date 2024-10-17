9fecb58ae2fb7877e4964a76b17ce7b981c68619
skate090961 <m4xwork@yandex.ru>
2024-10-17 16:18:04 +0400

    upd: add new words

--- src/scripts/game/consts.ts
+++ src/scripts/game/consts.ts
@@ -1,4 +1,22 @@
-export const WORDS: string[] = ['code', 'frontend', 'programming'];
+export const WORDS: string[] = [
+  'code',
+  'frontend',
+  'programming',
+  'development',
+  'javascript',
+  'typescript',
+  'hangman',
+  'react',
+  'angular',
+  'vue',
+  'css',
+  'html',
+  'git',
+  'api',
+  'database',
+  'backend',
+  'devops',
+];
 
 export const KEYBOARD_LETTERS: string[] = Array.from(
   'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
